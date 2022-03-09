dc93f2bae45c2f4f2687889ccbb911a52e247f13
kovus380 <86646679+kovus380@users.noreply.github.com>
2022-03-10 08:15:54 +0900

    Time: 49 ms (38.41%), Space: 13.9 MB (75.24%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.py
+++ 771-jewels-and-stones/771-jewels-and-stones.py
@@ -1,8 +1,7 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        jewel = collections.defaultdict(int)
-        for j in  jewels:
-            jewel[j] = 1
+        jewel = set(jewels)
+        print(jewel)
         
         answer = [stone for stone in stones if stone in jewel]
         return len(answer)
