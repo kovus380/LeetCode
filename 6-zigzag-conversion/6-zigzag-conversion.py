674669f3e9a2a3c274488bfe612b280639e6bfc7
Chae-Yeon OK <86646679+kovus380@users.noreply.github.com>
2022-03-20 19:28:02 +0900

    Time: 130 ms (19.67%), Space: 14.3 MB (26.01%) - LeetHub

---
+++ 6-zigzag-conversion/6-zigzag-conversion.py
@@ -0,0 +1,26 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+        elif numRows == 2:
+            indexes = []
+            indexes += [s[index] for index in range(0, len(s), 2)]
+            indexes += [s[index] for index in range(1, len(s), 2)]
+            return ''.join(indexes)
+            
+        indexes = []
+        
+        repeat = numRows + (numRows - 2)
+        indexes_first = [index for index in range(0, len(s), repeat)]
+        print(indexes_first)
+        indexes_last = [index for index in range(numRows - 1, len(s), repeat)]
+        
+        indexes_middle = []
+        for start_index in range(1, numRows - 1):
+            for index in range(start_index, len(s), repeat):
+                indexes_middle += [index]
+                if index + (repeat - (2 * start_index)) < len(s):
+                    indexes_middle += [index + (repeat - (2 * start_index))]
+        print(indexes_first, indexes_middle, indexes_last)
+        return (''.join([s[index] for index in indexes_first] + [s[index] for index in indexes_middle] + [s[index] for index in indexes_last]))
+        
