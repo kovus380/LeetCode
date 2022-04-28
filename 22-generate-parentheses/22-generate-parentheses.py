2b679d6f8c508da90e997eed8cfc7cb90b8b6870
Chae-Yeon OK <86646679+kovus380@users.noreply.github.com>
2022-04-28 17:44:25 +0900

    Time: 32 ms (94.71%), Space: 14.2 MB (42.03%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -1,39 +1,11 @@
 class Solution:
-
-                
-    def generateParenthesis(self, n: int) -> List[str]:
-        
-        combs = []
-        def dfs(s, n, leave, depth):
-            leave_tmp = leave[:]
-            if depth == 2 * n:
-                combs.append(s)
-                print(s)
-                return 
-            if leave_tmp[0] == 0:
-                print(1, s)
-                s += ")"
-                leave_tmp[1] -= 1
-                dfs(s, n, leave_tmp, depth + 1)
-
-            else:
-                if leave_tmp[0] == leave_tmp[1]:
-                    print(2, s)
-                    s += "("
-                    leave_tmp[0] -= 1
-                    dfs(s, n, leave_tmp, depth + 1)
-                elif leave_tmp[0] < leave_tmp[1]:
-                    print(3, s)
-                    s += ")"
-                    leave_tmp[1] -= 1
-                    dfs(s, n, leave_tmp, depth + 1)
-                    
-                    print(4, s, leave_tmp)
-                    s = s[:-1] + "("
-                    leave_tmp[1] += 1
-                    leave_tmp[0] -= 1
-                    dfs(s, n, leave_tmp, depth + 1)
-        
-        
-        dfs("", n, [n, n], 0)
-        return combs
+    def generateParenthesis(self, n):
+        def generate(p, left, right, parens=[]):
+            if left:
+                generate(p + '(', left-1, right)
+            if right > left: 
+                generate(p + ')', left, right-1)
+            if not right:    
+                parens += p,
+            return parens
+        return generate('', n, n)
