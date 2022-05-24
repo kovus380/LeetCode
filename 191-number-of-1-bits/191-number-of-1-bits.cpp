2248eb3425dc28b3e0af368aaeb9db6d9d18ca1d
Chae-Yeon OK <86646679+kovus380@users.noreply.github.com>
2022-05-24 19:12:47 +0900

    Time: 3 ms (54.95%), Space: 5.9 MB (79.19%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        return bitset<32>(n).count();
+        int count = 0;
+        while (n > 0) {
+            count++;
+            n = n & (n - 1);
+        }
+        return count;
+        
     }
 };
