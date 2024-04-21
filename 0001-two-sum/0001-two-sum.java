4a294e118914a2869b085e9ececc1f2dee20d1e5
Chae-Yeon OK <86646679+kovus380@users.noreply.github.com>
2024-04-21 23:07:01 +0900

    Time: 46 ms (21.26%), Space: 44.6 MB (81.60%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,8 +4,7 @@ public class Solution {
         for (int i = 0 ; i < (nums.length - 1) ; i++ ) {
             for (int j = i + 1 ; j < (nums.length) ; j++ ) {
                 if ((nums[i] + nums[j]) == target) {
-                    int[] result = {i, j};
-                    return result;
+                    return new int[] {i, j};
                 }
             }
         }
